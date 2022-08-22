957cc88c5e092d8b492f97f7ab94d5a3278a1df5
Mike Whalen <mww@amazon.com>
2022-08-22 01:52:12 +0000

    Fixed a sizing issue with printing.

--- src/checker.cpp
+++ src/checker.cpp
@@ -561,7 +561,7 @@ void Checker::add_derived_clause (int64_t id, const vector<int64_t> * chain, con
     if (chain) {
       fputs ("\nchain:\n", stderr);
       for (const auto & cid : *chain) {
-        fprintf (stderr, "%lld: ",cid);
+        fprintf (stderr, "%ld: ",cid);
         auto c = find_id (cid);
         if (!*c) { fprintf (stderr, "not found\n"); continue; }
         int size = (*c)->size;
