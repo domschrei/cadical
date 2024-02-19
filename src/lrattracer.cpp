94c4ed82396f558088d8a8e41290e96c9fed0c31
Dominik Schreiber <mail@dominikschreiber.de>
2024-02-19 20:05:18 +0100

    lock() rather than try_lock() in CaDiCaL LRAT proof writing

--- src/lrattracer.cpp
+++ src/lrattracer.cpp
@@ -149,7 +149,7 @@ void LratTracer::lrat_delete_clause (uint64_t id) {
 void LratTracer::add_derived_clause (uint64_t id, bool redundant,
                                      const vector<int> &clause,
                                      const vector<uint64_t> &chain) {
-  if (!mtx_write.try_lock ()) return;
+  mtx_write.lock ();
   LOG ("LRAT TRACER tracing addition of derived clause");
   lrat_add_clause (id, redundant, clause, chain);
 #ifndef QUIET
